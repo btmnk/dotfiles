28784a61df086e0ea74807991b1a6f7db5c69e44
btmnk <contact@bitmonk.de>
2022-07-09 18:03:24 +0200

    Adds windows-terminal settings

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -36,21 +36,15 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 require('lspconfig')['pyright'].setup{
-    capabilities = capabilities,
     on_attach = on_attach,
     flags = lsp_flags,
 }
 require('lspconfig')['tsserver'].setup{
-    capabilities = capabilities,
     on_attach = on_attach,
     flags = lsp_flags,
 }
 require('lspconfig')['rust_analyzer'].setup{
-    capabilities = capabilities,
     on_attach = on_attach,
     flags = lsp_flags,
     -- Server-specific settings...
